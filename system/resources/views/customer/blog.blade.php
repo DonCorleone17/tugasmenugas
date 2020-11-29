449538cec526770615da152c94de7653d85f81a2
DonCorleone17 <mukebatu@gmail.com>
2020-11-30 04:48:28 +0700

    Initial commit

---
+++ system/resources/views/customer/blog.blade.php
@@ -0,0 +1,188 @@
+@extends('customer.base')
+
+@section('content')
+    <section class="ftco-section ftco-degree-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 ftco-animate">
+            <div class="row">
+              <div class="col-md-12 d-flex ftco-animate">
+                <div class="blog-entry align-self-stretch d-md-flex">
+                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');">
+                  </a>
+                  <div class="text d-block pl-md-4">
+                    <div class="meta mb-3">
+                      <div><a href="#">July 20, 2019</a></div>
+                      <div><a href="#">Admin</a></div>
+                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                    </div>
+                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
+                    <p>Yeh dil dhadka do, zulfen bikhraa do. Sharmaake apna, aanchal lehraa do. Hum zulfen to bikhraa de, din mein raat na ho jaaye. Hum aanchal to lehraa de, par barsaat na ho jaaye.</p>
+                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-12 d-flex ftco-animate">
+                <div class="blog-entry align-self-stretch d-md-flex">
+                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_2.jpg');">
+                  </a>
+                  <div class="text d-block pl-md-4">
+                    <div class="meta mb-3">
+                      <div><a href="#">July 20, 2019</a></div>
+                      <div><a href="#">Admin</a></div>
+                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                    </div>
+                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
+                    <p>Yeh dil dhadka do, zulfen bikhraa do. Sharmaake apna, aanchal lehraa do. Hum zulfen to bikhraa de, din mein raat na ho jaaye. Hum aanchal to lehraa de, par barsaat na ho jaaye.</p>
+                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-12 d-flex ftco-animate">
+                <div class="blog-entry align-self-stretch d-md-flex">
+                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');">
+                  </a>
+                  <div class="text d-block pl-md-4">
+                    <div class="meta mb-3">
+                      <div><a href="#">July 20, 2019</a></div>
+                      <div><a href="#">Admin</a></div>
+                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                    </div>
+                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
+                    <p>Yeh dil dhadka do, zulfen bikhraa do. Sharmaake apna, aanchal lehraa do. Hum zulfen to bikhraa de, din mein raat na ho jaaye. Hum aanchal to lehraa de, par barsaat na ho jaaye.</p>
+                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-12 d-flex ftco-animate">
+                <div class="blog-entry align-self-stretch d-md-flex">
+                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_4.jpg');">
+                  </a>
+                  <div class="text d-block pl-md-4">
+                    <div class="meta mb-3">
+                      <div><a href="#">July 20, 2019</a></div>
+                      <div><a href="#">Admin</a></div>
+                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                    </div>
+                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
+                    <p>Yeh dil dhadka do, zulfen bikhraa do. Sharmaake apna, aanchal lehraa do. Hum zulfen to bikhraa de, din mein raat na ho jaaye. Hum aanchal to lehraa de, par barsaat na ho jaaye.</p>
+                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-12 d-flex ftco-animate">
+                <div class="blog-entry align-self-stretch d-md-flex">
+                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_5.jpg');">
+                  </a>
+                  <div class="text d-block pl-md-4">
+                    <div class="meta mb-3">
+                      <div><a href="#">July 20, 2019</a></div>
+                      <div><a href="#">Admin</a></div>
+                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                    </div>
+                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
+                    <p>Yeh dil dhadka do, zulfen bikhraa do. Sharmaake apna, aanchal lehraa do. Hum zulfen to bikhraa de, din mein raat na ho jaaye. Hum aanchal to lehraa de, par barsaat na ho jaaye.</p>
+                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-12 d-flex ftco-animate">
+                <div class="blog-entry align-self-stretch d-md-flex">
+                  <a href="blog-single.html" class="block-20" style="background-image: url('images/image_6.jpg');">
+                  </a>
+                  <div class="text d-block pl-md-4">
+                    <div class="meta mb-3">
+                      <div><a href="#">July 20, 2019</a></div>
+                      <div><a href="#">Admin</a></div>
+                      <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                    </div>
+                    <h3 class="heading"><a href="#">Inim bah ye contohnyen</a></h3>
+                    <p>Yeh dil dhadka do, zulfen bikhraa do. Sharmaake apna, aanchal lehraa do. Hum zulfen to bikhraa de, din mein raat na ho jaaye. Hum aanchal to lehraa de, par barsaat na ho jaaye.</p>
+                    <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Baca selanjutnya</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> <!-- .col-md-8 -->
+          <div class="col-lg-4 sidebar ftco-animate">
+            <div class="sidebar-box">
+              <form action="#" class="search-form">
+                <div class="form-group">
+                  <span class="icon ion-ios-search"></span>
+                  <input type="text" class="form-control" placeholder="Search...">
+                </div>
+              </form>
+            </div>
+            <div class="sidebar-box ftco-animate">
+              <h3 class="heading">Kategori</h3>
+              <ul class="categories">
+                <li><a href="#">Barang<span>(12)</span></a></li>
+                <li><a href="#">Jasa<span>(22)</span></a></li>
+                <li><a href="#">Toko<span>(37)</span></a></li>
+                <li><a href="#">Pemilu Bupati 2020<span>(42)</span></a></li>
+              </ul>
+            </div>
+
+            <div class="sidebar-box ftco-animate">
+              <h3 class="heading">Baru-baru ini</h3>
+              <div class="block-21 mb-4 d-flex">
+                <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
+                <div class="text">
+                  <h3 class="heading-1"><a href="#">Bukan gak cakap sombong, biase mah yak macam nyan te.</a></h3>
+                  <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span> April 09, 2019</a></div>
+                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="block-21 mb-4 d-flex">
+                <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
+                <div class="text">
+                  <h3 class="heading-1"><a href="#">Bukan gak cakap sombong, biase mah yak macam nyan te.</a></h3>
+                  <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span> April 09, 2019</a></div>
+                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="block-21 mb-4 d-flex">
+                <a class="blog-img mr-4" style="background-image: url(images/image_3.jpg);"></a>
+                <div class="text">
+                  <h3 class="heading-1"><a href="#">Bukan gak cakap sombong, biase mah yak macam nyan te.</a></h3>
+                  <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span> April 09, 2019</a></div>
+                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="sidebar-box ftco-animate">
+              <h3 class="heading">Tag</h3>
+              <div class="tagcloud">
+                <a href="#" class="tag-cloud-link">Martin Rantan</a>
+                <a href="#" class="tag-cloud-link">Junaidi</a>
+                <a href="#" class="tag-cloud-link">Sahrani</a>
+                <a href="#" class="tag-cloud-link">Eryanto Harun</a>
+                <a href="#" class="tag-cloud-link">Mateus Yudi</a>
+                <a href="#" class="tag-cloud-link">Farhan</a>
+                <a href="#" class="tag-cloud-link">Iin Solinar</a>
+                <a href="#" class="tag-cloud-link">Rahmad Sutoyo</a>
+              </div>
+            </div>
+
+            <div class="sidebar-box ftco-animate">
+              <h3 class="heading">Sekapur Sirih</h3>
+              <p>Tendang terajang tingkap legap, tesepak ke kayu usah sememadi. Intinye jangan banyak cakap, bende gini maok jadi. <br> Adil ka' talino, bacuramin ka' saruga, basengat ka' jubata!</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section> <!-- .section -->
+
+    
+@endsection
---
+++ system/resources/views/template/section/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Marketplace Terpercaya Untuk Anda
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020 @if(date("Y") > '2020') - {{date("Y")}} @endif <a href="https://www.instagram.com/rizkyorama/">BANGSAT.id | Rizky Orama</a>.</strong> Hak Cipta Dilindungi.
+  </footer>
